a0f930a9bd91fe4960cb77f4bda59a999b55d4d4
Dhairya Thareja <dhairyathareja@gmail.com>
2026-04-04 15:46:02 +0530

    Code Update to test Pipeline

--- my-app/app.py
+++ my-app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from CI/CD Pipeline!"
-    #return "Hello from CI/CD Pipeline!, my sapid is 123456"
+    # return "Hello from CI/CD Pipeline!"
+    return "Hello from CI/CD Pipeline!, my sapid is 500118984"
 
 app.run(host="0.0.0.0", port=80)
